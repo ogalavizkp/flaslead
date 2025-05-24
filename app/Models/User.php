3fca0bbd865455463a4ef787fd4460251efd454b
Omar Galaviz <ogalaviz@kiwanoplus.com>
2025-05-23 19:06:57 -0500

    add panel y filamenuser en model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable implements MustVerifyEmail
+
+class User extends Authenticatable implements FilamentUser
 {
     public function canAccessPanel(Panel $panel): bool
     {
